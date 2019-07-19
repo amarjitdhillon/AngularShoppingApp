05b095cb2bb2690940543a09b1fc583061fc30f0
Amarjit Dhillon <amarjit314@gmail.com>
2019-07-19 03:44:36 -0400

    Added various componets related to recepies and shopping list

---
+++ src/app/recepies/recepie-detail/recepie-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recepie-detail',
+  templateUrl: './recepie-detail.component.html',
+  styleUrls: ['./recepie-detail.component.sass']
+})
+export class RecepieDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recepie-item',
+  templateUrl: './recepie-item.component.html',
+  styleUrls: ['./recepie-item.component.sass']
+})
+export class RecepieItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
